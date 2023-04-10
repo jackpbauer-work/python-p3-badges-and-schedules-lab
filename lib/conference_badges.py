531e29b3a87c4632241d2821de4a2c036c4c9911
jackpbauer-work <124083011+jackpbauer-work@users.noreply.github.com>
2023-04-10 13:19:51 -0400

    badges and schedules

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,17 @@
 def badge_maker(name):
-    return None
+    return 'Hello, my name is ' + name + '.'
 
 def batch_badge_creator(names):
-    return None
+    return [badge_maker(name) for name in names]
 
 def assign_rooms(names):
-    return None
+    room_assignments = []
+    for i, name in enumerate(names):
+        room_assignments.append('Hello, ' + name + '! You\'ll be assigned to room ' + str(i+1) + '!')
+    return room_assignments
 
 def printer(names):
-    return None
+    for badge in batch_badge_creator(names):
+        print(badge)
+    for room_assignment in assign_rooms(names):
+        print(room_assignment)
